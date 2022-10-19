abfe5a0fb2d3f21a2d44724c9651b4a2438f82e9
Maxim Savenkov <max_savenkov@mail.ru>
2022-10-19 20:55:40 +0200

    Initial rust stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#![allow(warnings)]
+use clap::Parser;
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Parser)]
+struct Cli {
+    /// The path to the file to read
+    path: PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let mut file = File::open(args.path).expect("Cannot open file!");
+    let mut code: String = "".to_string();
+    file.read_to_string(&mut code).expect("File cannot be read");
+    println!("{}", code);
+
+    //Lexer
+
+    //Parse into AST
+
+    //LLVM JIT compile
+
 }
